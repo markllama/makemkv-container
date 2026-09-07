fec3df4ca1a8e7692b86bce32c5ae909b5ea6fce
Mark Lamourine <mark@llamatek.dev>
2026-09-07 13:52:24 -0400

    resolve libs for tools too

--- scripts/minimal-makemkv.sh
+++ scripts/minimal-makemkv.sh
@@ -18,11 +18,14 @@ DEFAULT_SERVICE="makemkvcon"
 DEFAULT_SOURCE_ROOT="model"
 DEFAULT_AUTHOR="Mark Lamourine <markllama@gmail.com>"
 DEFAULT_BUILDER="Mark Lamourine <markllama@gmail.com>"
+DEFAULT_BASE=scratch
 
 : SERVICE="${SERVICE:=${DEFAULT_SERVICE}}"
 : SOURCE_ROOT="${SOURCE_ROOT:=${DEFAULT_SOURCE_ROOT}}"
 : AUTHOR="${AUTHOR:=${DEFAULT_AUTHOR}}"
 : BUILDER="${BUILDER:=${DEFAULT_BUILDER}}"
+: BASE="${BASE:=${DEFAULT_BASE}}":
+
 
 function main() {
 
@@ -30,19 +33,20 @@ function main() {
 
     if [ -z "${BUILDAH_ISOLATION}" -o -z "${CONTAINER_ID}" ] ; then
 	# Create a container
-	local container=$(buildah from --name $SERVICE scratch)
+	local container=$(buildah from --name $SERVICE ${BASE})
 
 	if [ -z "${BUILDAH_ISOLATION}" ] ; then
 	    # Run the file copy in an unshare environement
 	    buildah unshare bash $0 -c ${container} -s ${SOURCE_ROOT}
 	else
-	    # Aldready in an unshare environment
+	    # Already in an unshare environment
 	    copy_model_tree ${SOURCE_ROOT} ${container}
 	fi
 	
 	# add a volume to include the configuration file
 	# Leave the files in the default locations 
 	#buildah config --env MAKEMKV_KEY $container
+	buildah config --env "LD_LIBRARY_PATH=/usr/lib64:/usr/lib" $container
 	buildah config --volume /keyfile $container
 	buildah config --volume /data/input $container
 	buildah config --volume /data/output $container
@@ -51,8 +55,8 @@ function main() {
 #	buildah config --port 68/udp --port 69/udp ${container}
 
 	# # Define the startup command
-	#buildah config --entrypoint /usr/bin/bash $container
-	buildah config --cmd "/usr/bin/makemkvcon" $container
+	#buildah config --entrypoint "/usr/bin/bash" $container
+	#buildah config --cmd "/usr/bin/makemkvcon" $container
 
 	buildah config --author "${AUTHOR}" $container
 	buildah config --created-by "${BUILDER}" $container
@@ -73,25 +77,30 @@ function main() {
 }
 
 function copy_model_tree() {
+    set -x
     local source_root=$1
     local container_id=$2
     
     # Access the container file space
-    local mountpoint=$(buildah mount $container_id)
+    local mountpoint=$(buildah mount ${container_id})
 
+    [ -z ${DEBUG} ] || (echo EMPTY ; cd ${mountpoint} ; pwd ; find . -type f)
+
+    
     # Create the model directory tree
     (cd ${source_root} ; find * -type d) | xargs -I{} mkdir -p ${mountpoint}/{}
-    [ -z "${DEBUG}" ] || ls -R ${mountpoint}
+
     cp -r ${source_root}/* ${mountpoint}
     
     # Create volume mount points
     mkdir -p ${mountpoint}/data/input
     mkdir -p ${mountpoint}/data/output
 
-    [ -z ${DEBUG} ] || ls -R ${mountpoint}
+    [ -z ${DEBUG} ] || (echo FULL ; cd ${mountpoint} ; pwd ;  find . -type f)
 
     # Release the container file space
     buildah unmount ${container_id}
+    set +x
 }
 
 function parse_args() {
--- scripts/resolve-shared-libraries.sh
+++ scripts/resolve-shared-libraries.sh
@@ -7,12 +7,19 @@
 : UNPACK_ROOT=${UNPACK_ROOT:=unpack}
 function main() {
 
+    #dnf install -yes --installroot ${MODEL_ROOT} -- setopt install_weak_deps=false 
     [ -s ${MODEL_ROOT}/bin ] || ln -s usr/bin ${MODEL_ROOT}/bin
-    [ -s ${MODEL_ROOT}/lib ] || ln -s usr/lib ${MODEL_ROOT}/lib
+    [ -s ${MODEL_ROOT}/lib ] || ln -s usr/lib ${MODEL_ROOT}/lib64
     [ -s ${MODEL_ROOT}/lib64 ] || ln -s usr/lib64 ${MODEL_ROOT}/lib64
+    [ -s ${MODEL_ROOT}/usr/lib ] || ln -s lib64 ${MODEL_ROOT}/usr/lib
     
     rm -rf ${PACKAGE_ROOT}/*
     rm -rf ${UNPACK_ROOT}/*
+
+    # Add debugging tools: bash and ldd
+    cp /usr/bin/bash ${MODEL_ROOT}/usr/bin/bash
+    ln -s bash ${MODEL_ROOT}/usr/bin/sh
+    cp /usr/bin/ldd ${MODEL_ROOT}/usr/bin/ldd
 
     local binaries=$(find_dynamic_binaries ${MODEL_ROOT})
 
@@ -49,10 +56,8 @@ function main() {
 	cp ${UNPACK_ROOT}/${library_file} ${MODEL_ROOT}/${library_file}
     done
 
-    # Add debugging tools: bash and ldd
-    cp /usr/bin/bash ${MODEL_ROOT}/usr/bin/bash
-    ln -s bash ${MODEL_ROOT}/usr/bin/sh
-    cp /usr/bin/ldd ${MODEL_ROOT}/usr/bin/ldd
+    cp ${UNPACK_ROOT}/lib64/ld-linux-* ${MODEL_ROOT}/lib64
+    
 }
 
 function find_dynamic_binaries() {
